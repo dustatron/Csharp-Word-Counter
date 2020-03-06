21b8c0a4af22cd36eb657809c2e904100eafb57f
Dusty <dustymccord@gmail.com>
2020-03-06 09:30:56 -0800

    add passing test for Validate Method.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,11 +5,14 @@ namespace Word.Models
   //change name of class.
   public class Counter
   {
+    private static char[] _specialChar = new Char[] {'!', '@', '#', '$', '^', '&', '*', '(', ')', '_', '-', '=', '+', ',', '.', '\\', '/', '`', '~', ':', ';', '{', '}', '[', ']', '|', '?', '<', '>' };
     //Change the name of the method.
     public static string Validate(string word)
     {
-      string result = "";
-      return result;
+      //Remove special Characters
+      string result = word.Trim(_specialChar);
+
+      return result.ToLower();
     }
   }
 
