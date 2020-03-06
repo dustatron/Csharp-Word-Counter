853be4b8ce4a1cf6d758fa0664b573f392ca810e
Dusty <dustymccord@gmail.com>
2020-03-06 14:34:26 -0800

    add a basic user interface.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -10,41 +10,36 @@ namespace Word
   {
     public static void Main()
     {
-      Console.BackgroundColor = ConsoleColor.Blue;
-      Console.Clear();
-      Console.WriteLine(@"
-                              ____    __    ____  ______   .______       _______                 
-                              \   \  /  \  /   / /  __  \  |   _  \     |       \                
-                               \   \/    \/   / |  |  |  | |  |_)  |    |  .--.  |               
-                                \            /  |  |  |  | |      /     |  |  |  |               
-                                 \    /\    /   |  `--'  | |  |\  \----.|  '--'  |               
-                                  \__/  \__/     \______/  | _| `._____||_______/                
-                                                                                                 
-                       ______   ______    __    __  .__   __. .___________. _______ .______      
-                      /      | /  __  \  |  |  |  | |  \ |  | |           ||   ____||   _  \     
-                     |  ,----'|  |  |  | |  |  |  | |   \|  | `---|  |----`|  |__   |  |_)  |    
-                     |  |     |  |  |  | |  |  |  | |  . `  |     |  |     |   __|  |      /     
-                     |  `----.|  `--'  | |  `--'  | |  |\   |     |  |     |  |____ |  |\  \----.
-                      \______| \______/   \______/  |__| \__|     |__|     |_______|| _| `._____|
-                                                                                                                                   
-      ");
-      Copy.Type("Hello and welcome to the world famus word counter!", 35, true);
-      Copy.Type("This program can tell you how many times a word shows up in a sentence.", 35, true);
-      System.Console.WriteLine();
+      string inputSentence;
+      string inputWord;
 
-      Copy.Type("Enter your sentence...", 50, false);
-      string inputSentence = Console.ReadLine();
-      System.Console.WriteLine();
-      Copy.Type("Enter your word...", 50, false);
-      string inputWord = Console.ReadLine();
-      System.Console.WriteLine();
+      Copy.FirstRun(out inputSentence, out inputWord);
 
-      Copy.Type("we are now using your advanced algorithms to process your request", 10, true);
-      Copy.Type("Processing . ", 50, false);
-      Copy.Type(". . . . . . .", 500, true);
-      Copy.Type("Completed...", 50, true);
-      string test = Console.ReadLine();
+      //run program
 
+      int result = 0;
+      try
+      {
+        result = Counter.ReturnCount(inputSentence, inputWord);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+
+      Copy.Readout(result, inputSentence, inputWord);
+      string endMenu = Console.ReadLine();
+
+      if (endMenu.ToLower() == "q")
+      {
+        Copy.isStaying(false);
+      }
+      else
+      {
+        Copy.isStaying(true);
+        Main();
+
+      }
 
 
     }
--- WordCounter/Views/MainView.cs
+++ WordCounter/Views/MainView.cs
@@ -26,5 +26,67 @@ namespace Word.Views
 
     }
 
+    public static void FirstRun(out string inputSentence, out string inputWord)
+    {
+      Console.BackgroundColor = ConsoleColor.Blue;
+      Console.Clear();
+      Console.WriteLine(@"
+                              ____    __    ____  ______   .______       _______                 
+                              \   \  /  \  /   / /  __  \  |   _  \     |       \                
+                               \   \/    \/   / |  |  |  | |  |_)  |    |  .--.  |               
+                                \            /  |  |  |  | |      /     |  |  |  |               
+                                 \    /\    /   |  `--'  | |  |\  \----.|  '--'  |               
+                                  \__/  \__/     \______/  | _| `._____||_______/                
+                                                                                                 
+                       ______   ______    __    __  .__   __. .___________. _______ .______      
+                      /      | /  __  \  |  |  |  | |  \ |  | |           ||   ____||   _  \     
+                     |  ,----'|  |  |  | |  |  |  | |   \|  | `---|  |----`|  |__   |  |_)  |    
+                     |  |     |  |  |  | |  |  |  | |  . `  |     |  |     |   __|  |      /     
+                     |  `----.|  `--'  | |  `--'  | |  |\   |     |  |     |  |____ |  |\  \----.
+                      \______| \______/   \______/  |__| \__|     |__|     |_______|| _| `._____|
+                                                                                                                                   
+      ");
+      Type("Hello and welcome to the world famus word counter!", 35, true);
+      Type("This program can tell you how many times a word shows up in a sentence.", 35, true);
+      System.Console.WriteLine();
+
+      Type("Enter your sentence...", 50, false);
+      inputSentence = Console.ReadLine();
+      System.Console.WriteLine();
+      Type("Enter your word...", 50, false);
+      inputWord = Console.ReadLine();
+      System.Console.WriteLine();
+
+      Console.WriteLine("we are now using your advanced algorithms to process your request");
+      Type("Processing . ", 50, false);
+      Type(". . .", 400, true);
+      Type("Completed...", 50, true);
+    }
+
+    public static void Readout(int result, string sentence, string word)
+    {
+      string times = "times";
+      if (result == 1)
+      {
+        times = "time";
+      }
+      Type($"Your sentence was {sentence.Length} character long.", 50, true);
+      Type($"It included the word {word} {result} {times}.", 50, true);
+      Type($"Would you like to try again?", 50, true);
+      Type($"Press [Y] for yes or [Q] to quit.", 50, true);
+    }
+
+    public static void isStaying(bool staying)
+    {
+      if (staying)
+      {
+        Copy.Type("Great Lets do it again!!!        ", 50, true);
+      }
+      else
+      {
+        Copy.Type("It was good hanging out... See you next time     ", 50, true);
+      }
+    }
+
   }
 }
